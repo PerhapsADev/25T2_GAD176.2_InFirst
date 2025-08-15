a53cbb30bf89f5cb0eed812958db18d6fbc5fd27
PerhapsADev <leighton.stack.edu@gmail.com>
2025-08-16 00:51:04 +1000

    Prefab Update, audio added

--- Assets/Assets/Scripts/Turret.cs
+++ Assets/Assets/Scripts/Turret.cs
@@ -12,13 +12,27 @@ namespace ReusableStealthFramework.enemies
         [SerializeField] protected float accuracy = 100f;
         [SerializeField] public int damageValue = 1;
         [SerializeField] public float fireRate = 1f;
+        [SerializeField] public bool hasTarget = false;
+
+        [SerializeField] public AudioClip beeping;
+        [SerializeField] public AudioClip alarm;
+        [SerializeField] public AudioSource soundEffect;
 
         protected override void TargetSpotted()
         {
             // Debug.Log("Attempting to shoot");
             base.TargetSpotted();
 
+            if (!soundEffect.isPlaying)
+            {
+                soundEffect.Play();
+            }
 
+            if (!hasTarget)
+            {
+                soundEffect.PlayOneShot(alarm);
+                hasTarget = true;
+            }
             RaycastHit hit;
 
             Vector3 forward = this.transform.forward + RandomSpread();
@@ -36,19 +50,29 @@ namespace ReusableStealthFramework.enemies
                     player.TakesDamage();
                 }
             }
-        } 
+        }
+
+        protected override void SearchingForTarget()
+        {
+            base.SearchingForTarget();
+            if (rotationOffsetAmount >= rotationLimitPointRight || rotationOffsetAmount <= rotationLimitPointLeft)
+            {
+                soundEffect.PlayOneShot(beeping);
+            }
+        }
+
         public Vector3 RandomSpread()
 
         {
             float remainingPoints = 100f - accuracy;
-            float xAccuracyPosition = Random.Range(- remainingPoints, remainingPoints) / 1000f;
-            float yAccuracyPosition = Random.Range(- remainingPoints, remainingPoints) / 1000f;
-            float zAccuracyPosition = Random.Range(- remainingPoints, remainingPoints) / 1000f;
+            float xAccuracyPosition = Random.Range(-remainingPoints, remainingPoints) / 1000f;
+            float yAccuracyPosition = Random.Range(-remainingPoints, remainingPoints) / 1000f;
+            float zAccuracyPosition = Random.Range(-remainingPoints, remainingPoints) / 1000f;
 
             // Debug.Log (new Vector3(xAccuracyPosition, yAccuracyPosition, zAccuracyPosition));
             return new Vector3(xAccuracyPosition, yAccuracyPosition, zAccuracyPosition);
 
-        
+
         }
     }
 
